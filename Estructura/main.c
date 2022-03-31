0a6b7ac411bf3b61857c43094b30b69069e8f6c9
Jose-de-la-Barra <jose.delabarra2@gmail.com>
2022-03-31 10:16:40 -0300

    encuantra los libros y muestra la información del libro enccontrado por pantalla

--- Estructura/main.c
+++ Estructura/main.c
@@ -3,11 +3,13 @@
 #include<stdbool.h>
 #include "libros.h"
 #define MAXCHAR 1000
+#define LENGHT 250
 
 #define READ_ONLY "r"
+#define WRITE_ONLY "w"
 
-void showContentsAsStruct(FILE *pFile);
-void showContents(FILE *myFile);
+void searchBook(FILE *fp);
+
 
 FILE * openingFile(char *filename){
     FILE *fp;
@@ -17,45 +19,79 @@ FILE * openingFile(char *filename){
 
 
 
-int main(int argc, char *argv[] ){
+int main(int argc, char *argv[] ) {
 
     FILE *fp = openingFile(argv[1]);
+    if (fp == NULL) {
+        perror("Opening file");
+        return -1;
+    }
 
-    showContents(fp);
+    int option;
+    do
+    {
+        printf( "   1. Agregar libros\n");
+        printf( "   2. Quitar libros\n");
+        printf( "   3. Editar libros\n");  // cambiar sede, sección o piso
+        printf( "   4. Buscar\n");
+        printf( "   5. Salir.\n" );
+        printf( "   Introduzca opción (1-5): \n");
 
+        scanf("%d", &option);
+        if (option == 4) {
+            searchBook(fp);
+        }
+    } while ( option != 5 );
 
+    fclose(fp);
     return 0;
 
 }
 
-void showContentsAsStruct(FILE *pFile) {
-    Libro *libros;
-    //paso el contenido del file a un array de personas
-    libros = getLibro(pFile);
-
-    //accedo a la cantidad de registro que lei
-    for (int i = 0; i < registryCount; ++i) {
-        //puedo solo imprimir su mail por ejemplo
-        printf("%s \n", libros[i].titulo);
-    }
-
-}
-
-
-void showContents(FILE *myFile) {
+void searchBook(FILE *fp) {
     char row[MAXCHAR];
     char *token;
-    //muestra el contenido del archivo
-    while (feof(myFile) != true){
+    char title[LENGHT];
+
+    printf("\nIngresa el título del libro que quieres buscar:");
+    fpurge(stdin);  // para limpiar el buffer de entrada stdin
+    fgets(title, LENGHT, stdin);
+    strtok(title, "\n");
+
+    bool found = false;
+
+
+    while (feof(fp) != true) {
+
+        bool found2 = false;
         //obtiene la linea siguiente
-        fgets(row, MAXCHAR, myFile);
+        fgets(row, MAXCHAR, fp);
         token = strtok(row, ",");
-        //print id first
-        printf("Row: %s \n", token);
+
         for (int i = 0; i < 8; ++i) {
-            printf("Token %d: %s\n", i, token);
+            if (i == 0) {
+                if (strcmp(row, title) == 0) {
+                    printf("Encontramos el libro!\n");
+                    found = true;
+                    found2 = true;
+                }
+            }
+            if (found2 == true) {
+                printf("%s\n", token);
+            }
             token = strtok(NULL, ",");
         }
-
+    }
+    if (found == false) {
+        printf("Lo sentimos, su libro no se ha encontrado.\n");
     }
 }
+
+char* askStr(char* name[LENGHT]) {
+
+
+    return *name;
+}
+
+
+
